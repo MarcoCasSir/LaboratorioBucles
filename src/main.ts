cc8491df4c6d97ed57c52852df5a150717e138f4
PORTATIL_MARCO\marco <marco.casati74@gmail.com>
2025-09-15 12:28:22 +0200

    apartado 2

--- src/main.ts
+++ src/main.ts
@@ -111,3 +111,24 @@ const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (
 };
 
 console.log(obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes));
+
+// Apartado 2
+// Activar protocolo de URGENCIA
+
+const activarProtocoloUrgencia = (pacientes: Pacientes[]): boolean => {
+  let activarProtocolo = false;
+
+  for (let i = 0; i < pacientes.length; i++) {
+    if (
+      pacientes[i].frecuenciaCardiaca > 100 ||
+      pacientes[i].temperatura > 39
+    ) {
+      activarProtocolo = true;
+      break;
+    }
+  }
+
+  return activarProtocolo;
+};
+
+console.log(activarProtocoloUrgencia(pacientes));
